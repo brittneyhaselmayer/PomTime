f02a191c810821d05a646c3e74584b15c1ec56e7
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2020-07-24 17:28:10 -0500

    timer div

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Login from './Login';
+import Timer from './Timer';
+import '../styles/Home.css';
 
 export default function Home() {
 	return (
-		<div>
-			<Login />
+		<div className="homeDiv">
+			<Login className="login" />
+			<Timer className="timer" />
 		</div>
 	);
 }
---
+++ src/components/Timer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/Timer.css';
+
+export default function Timer() {
+	return (
+		<div className="timerDiv">
+			<h1>10:00min</h1>
+			<button>Start timer</button>
+		</div>
+	);
+}
