2ce81ca05a02db371432698cfaf2ebbdba4a6639
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2020-07-25 22:17:29 -0500

    starting timer func

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Timer.css';
 
 export default function Timer() {
+	const [time, setTime] = useState('25:00');
+	const [value, setValue] = useState('');
+	// console.log(time);
+
+	const getTimeValue = (e) => {
+		e.preventDefault();
+		e.persist();
+		setTime(value);
+	};
+
+	const handleChange = (e) => {
+		setValue(e.target.value);
+	};
+
 	return (
 		<div className="timerDiv">
-			<h1>10:00min</h1>
+			<h1>{time} Mins</h1>
+
 			<button>Start timer</button>
+			<form>
+				<input type="text" onChange={(e) => handleChange(e)}></input>
+				<button onClick={(e) => getTimeValue(e)}>Set Custom Time</button>
+			</form>
 		</div>
 	);
 }
